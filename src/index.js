f701cd90c3b135e7fb27eb661a46d43f769a2ee4
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2019-10-17 18:29:35 +0300

    Complete the brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var arr = str.split("");
+    if (arr.length % 2 !== 0) return false;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if ((arr[i] === bracketsConfig[j][0]) && (arr[i + 1] === bracketsConfig[j][1])) {
+                arr.splice(i, 2);
+                i = -1;
+            } else {
+                continue;
+            }
+        }
+    }
+    if (arr.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
